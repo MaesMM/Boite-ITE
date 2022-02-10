d96d857d5e261a25bb050a4170c4b3afa5444db1
Kotalos <92145863+Kotalos@users.noreply.github.com>
2022-02-10 10:38:55 +0100

    API v2

--- server/main/api/models.py
+++ server/main/api/models.py
@@ -1,5 +1,3 @@
-from gc import collect
-from types import BuiltinFunctionType
 from uuid import uuid4
 from django.db import models
 
@@ -7,15 +5,15 @@ from django.db import models
 class Building(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     #user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    name = models.CharField(max_length=32)
+    name = models.CharField(max_length=32, unique=True)
 
     def __str__(self):
         return self.name
 
 class Room(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    building = models.ForeignKey(Building, on_delete=models.CASCADE)
-    name = models.CharField(max_length=32)
+    building = models.ForeignKey(Building, related_name='rooms', on_delete=models.CASCADE)
+    name = models.CharField(max_length=32, unique=True)
 
     color = models.CharField(max_length=16)
     collect_frequency = models.IntegerField(default=300)
--- server/main/api/serializers.py
+++ server/main/api/serializers.py
@@ -2,12 +2,22 @@ from dataclasses import field
 from rest_framework import serializers
 from .models import *
 
-class BuildingSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Building
-        fields = ['uuid', 'name']
-
 class RoomSerializer(serializers.HyperlinkedModelSerializer):
+
+    building = serializers.PrimaryKeyRelatedField(queryset=Building.objects.all(), source='building.uuid')
+
     class Meta:
         model = Room
-        fields = ['uuid', 'building', 'name', 'color', 'collect_frequency']
+        fields = ['uuid', 'url', 'building', 'name', 'color', 'collect_frequency']
+        # depth = 1
+
+    def create(self, validated_data):
+        subject = Room.objects.create(building=validated_data['building']['uuid'], name=validated_data['name'], color=validated_data['color'], collect_frequency=validated_data['collect_frequency'])
+        return subject
+
+class BuildingSerializer(serializers.HyperlinkedModelSerializer):
+    rooms = RoomSerializer(many=True, required=False)
+    
+    class Meta:
+        model = Building
+        fields = ['uuid', 'url', 'name', 'rooms']
